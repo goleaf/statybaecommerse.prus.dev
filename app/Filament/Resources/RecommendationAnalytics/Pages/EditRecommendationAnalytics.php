cce0edcfaab53314d908978a59e4ae7ab834d752
Andrej Prus <goleaf@gmail.com>
2025-10-20 18:22:31 +0300

    feat: enhance recommendation analytics resource

--- app/Filament/Resources/RecommendationAnalytics/Pages/EditRecommendationAnalytics.php
+++ app/Filament/Resources/RecommendationAnalytics/Pages/EditRecommendationAnalytics.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\RecommendationAnalytics\Pages;
 
 use App\Filament\Resources\RecommendationAnalytics\RecommendationAnalyticsResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
-class EditRecommendationAnalytics extends EditRecord
+final class EditRecommendationAnalytics extends EditRecord
 {
     protected static string $resource = RecommendationAnalyticsResource::class;
 
