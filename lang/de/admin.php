dc13905ecec08a1a03fc02e4bca1802b8d53d6e6
Andrej Prus <goleaf@gmail.com>
2025-10-20 16:58:14 +0300

    Add Email Campaign admin translations

---
+++ lang/de/admin.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'email_campaigns' => require __DIR__.'/admin/email_campaigns.php',
+];
---
+++ lang/lt/admin/email_campaigns.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'navigation_label' => 'El. pašto kampanijos',
+    'plural_model_label' => 'El. pašto kampanijos',
+    'model_label' => 'El. pašto kampanija',
+    'basic_information' => 'Pagrindinė informacija',
+    'basic_information_description' => 'Sukonfigūruokite pagrindinius šios el. pašto kampanijos nustatymus.',
+    'name' => 'Pavadinimas',
+    'description' => 'Aprašymas',
+    'subject' => 'Tema',
+    'from_email' => 'Siuntėjo el. paštas',
+    'from_name' => 'Siuntėjo vardas',
+    'reply_to' => 'Atsakymo el. paštas',
+    'scheduled_at' => 'Numatytas siuntimo laikas',
+    'is_active' => 'Aktyvi',
+];
