89e3cbd5a4beadab6b8c409181e71b0c761ee33b
Andrej Prus <goleaf@gmail.com>
2025-10-20 17:39:21 +0300

    Fix channel relation manager forms and cleanup UnitEnum imports (#619)

--- app/Filament/Resources/Channels/RelationManagers/DiscountsRelationManager.php
+++ app/Filament/Resources/Channels/RelationManagers/DiscountsRelationManager.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\Channels\RelationManagers;
 
 use Filament\Actions\AssociateAction;
@@ -23,7 +25,7 @@ class DiscountsRelationManager extends RelationManager
     public function form(Form $form): Form
     {
         return $form
-            ->components([
+            ->schema([
                 TextInput::make('name')
                     ->required()
                     ->maxLength(255),
