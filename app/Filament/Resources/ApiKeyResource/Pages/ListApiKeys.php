4d2badc1c0985aac266a74bd1aea07ef8048c7a2
Andrej Prus <goleaf@gmail.com>
2025-10-20 12:37:11 +0300

    Add tests for Filament API key resource

---
+++ app/Filament/Resources/ApiKeyResource/Pages/ListApiKeys.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\ApiKeyResource\Pages;
+
+use App\Filament\Resources\ApiKeyResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+final class ListApiKeys extends ListRecords
+{
+    protected static string $resource = ApiKeyResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->label(__('api_keys.actions.create')),
+        ];
+    }
+}
