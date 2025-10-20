ac191e0891ec875cf6964929bfe8259dbc6e9774
Andrej Prus <goleaf@gmail.com>
2025-10-20 17:17:59 +0300

    Add product image component and align Filament metadata (#580)

---
+++ tests/Feature/Views/ProductImageComponentTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Views;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductImageComponentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_component_displays_provided_image(): void
+    {
+        $imagePath = 'product-images/example.jpg';
+
+        $view = $this->blade('<x-product-image :image="$path" alt="Preview" />', [
+            'path' => $imagePath,
+        ]);
+
+        $view->assertSee('product-images/example.jpg', false);
+        $view->assertSee('alt="Preview"', false);
+    }
+
+    public function test_component_displays_fallback_when_image_missing(): void
+    {
+        $view = $this->blade('<x-product-image :image="null" />');
+
+        $view->assertSeeText(__('admin.no_image'));
+    }
+}
