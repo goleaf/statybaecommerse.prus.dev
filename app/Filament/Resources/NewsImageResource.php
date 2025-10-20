96d428a4ed268ed5bb16f78179ebffb8aecf2df7
Andrej Prus <goleaf@gmail.com>
2025-10-20 16:59:18 +0300

    Fix no alt text filter grouping

--- app/Filament/Resources/NewsImageResource.php
+++ app/Filament/Resources/NewsImageResource.php
@@ -394,7 +394,9 @@ final class NewsImageResource extends Resource
                 Filter::make('no_alt_text')
                     ->label(__('admin.news_images.no_alt_text'))
                     ->query(fn (Builder $query): Builder => $query->where(
-                        fn (Builder $q): Builder => $q->whereNull('alt_text')->orWhere('alt_text', '')
+                        fn (Builder $q): Builder => $q
+                            ->whereNull('alt_text')
+                            ->orWhere('alt_text', '')
                     ))
                     ->toggle(),
             ])
