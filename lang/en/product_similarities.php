b323e6dc414f731a4f350d3ad17d8d6416fbb917
Andrej Prus <goleaf@gmail.com>
2025-10-20 18:16:07 +0300

    Implement ProductSimilarity Filament resource

---
+++ lang/en/product_similarities.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title'  => 'Product Similarities',
+    'plural' => 'Product Similarities',
+    'single' => 'Product Similarity',
+
+    'basic_information' => 'Similarity Details',
+
+    'product'          => 'Product',
+    'similar_product'  => 'Similar Product',
+    'algorithm_type'   => 'Algorithm Type',
+    'similarity_score' => 'Similarity Score',
+    'calculation_data' => 'Calculation Data',
+    'calculation_data_help' => 'Optional metadata describing how the similarity score was calculated.',
+    'calculated_at'    => 'Calculated At',
+    'created_at'       => 'Created At',
+    'updated_at'       => 'Updated At',
+
+    'filters' => [
+        'product'         => 'Product',
+        'similar_product' => 'Similar Product',
+        'algorithm_type'  => 'Algorithm Type',
+        'min_score'       => 'Minimum Score',
+        'max_score'       => 'Maximum Score',
+    ],
+
+    'algorithm_types' => [
+        'cosine'  => 'Cosine Similarity',
+        'jaccard' => 'Jaccard Similarity',
+    ],
+];
