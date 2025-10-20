ff75451f031cd023167d69f383503e4621b8c99e
Andrej Prus <goleaf@gmail.com>
2025-10-20 17:11:34 +0300

    Add UserResource view route and align framework compatibility

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -318,6 +318,7 @@ final class UserResource extends Resource
         return [
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
