1e0d75f6d412b1ba9f2c1830386caf49e2e68f11
Andrej Prus <goleaf@gmail.com>
2025-10-20 18:12:29 +0300

    Refine referral code usage log resource structure (#666)

--- app/Filament/Resources/ReferralCodeUsageLogs/Pages/ListReferralCodeUsageLogs.php
+++ app/Filament/Resources/ReferralCodeUsageLogs/Pages/ListReferralCodeUsageLogs.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\ReferralCodeUsageLogs\Pages;
 
 use App\Filament\Resources\ReferralCodeUsageLogs\ReferralCodeUsageLogResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
-class ListReferralCodeUsageLogs extends ListRecords
+final class ListReferralCodeUsageLogs extends ListRecords
 {
     protected static string $resource = ReferralCodeUsageLogResource::class;
 
