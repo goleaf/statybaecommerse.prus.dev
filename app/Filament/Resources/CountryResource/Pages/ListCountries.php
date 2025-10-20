0b0d5b1d5793676d05fe8ead8f2529d4be983bb2
Andrej Prus <goleaf@gmail.com>
2025-10-20 16:42:13 +0300

    Add country resource widgets and tests (#406)

--- app/Filament/Resources/CountryResource/Pages/ListCountries.php
+++ app/Filament/Resources/CountryResource/Pages/ListCountries.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 namespace App\Filament\Resources\CountryResource\Pages;
 
 use App\Filament\Resources\CountryResource;
+use App\Filament\Resources\CountryResource\Widgets\CountriesByRegionWidget;
+use App\Filament\Resources\CountryResource\Widgets\CountriesOverviewWidget;
+use App\Filament\Resources\CountryResource\Widgets\CountriesStatsWidget;
+use App\Filament\Resources\CountryResource\Widgets\CountriesWithVatWidget;
+use App\Filament\Resources\CountryResource\Widgets\CountryDetailsWidget;
+use App\Filament\Resources\CountryResource\Widgets\EuMembersWidget;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Components\Tab;
@@ -18,6 +24,24 @@ class ListCountries extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            CountriesOverviewWidget::class,
+            CountriesStatsWidget::class,
+            CountriesByRegionWidget::class,
+        ];
+    }
+
+    protected function getFooterWidgets(): array
+    {
+        return [
+            CountriesWithVatWidget::class,
+            EuMembersWidget::class,
+            CountryDetailsWidget::class,
         ];
     }
 
