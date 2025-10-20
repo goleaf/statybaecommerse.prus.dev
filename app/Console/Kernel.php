d4df9aeac181752193d840c1817a972547e2f96e
Andrej Prus <goleaf@gmail.com>
2025-10-20 11:22:13 +0300

    Add OpenAPI spec generation command (#73)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,7 @@ final class Kernel extends ConsoleKernel
         \App\Console\Commands\ValidateCodeStyleCommand::class,
         \App\Console\Commands\CodeStyleWatchCommand::class,
         \App\Console\Commands\DemonstrateTimeoutCommand::class,
+        \App\Console\Commands\GenerateApiSpecCommand::class,
         \App\Console\Commands\GenerateReportsCommand::class,
         \App\Console\Commands\CheckRefreshDatabaseCommand::class,
         \App\Console\Commands\BackupPrepareCommand::class,
