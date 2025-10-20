0a8815a0cfc4b37caf30e0566749c23c44ec497a
Andrej Prus <goleaf@gmail.com>
2025-10-20 18:21:23 +0300

    Refine recommendation cache resource

--- app/Filament/Resources/RecommendationCaches/Pages/ListRecommendationCaches.php
+++ app/Filament/Resources/RecommendationCaches/Pages/ListRecommendationCaches.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\RecommendationCaches\Pages;
 
 use App\Filament\Resources\RecommendationCaches\RecommendationCacheResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
-class ListRecommendationCaches extends ListRecords
+final class ListRecommendationCaches extends ListRecords
 {
     protected static string $resource = RecommendationCacheResource::class;
 
