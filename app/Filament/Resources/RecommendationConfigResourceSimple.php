245a15713c46638538d00e4b829338c0359dda6f
Andrej Prus <goleaf@gmail.com>
2025-10-21 15:32:53 +0300

    Use Novadaemon combobox component

--- app/Filament/Resources/RecommendationConfigResourceSimple.php
+++ app/Filament/Resources/RecommendationConfigResourceSimple.php
@@ -12,7 +12,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
-use Filament\Forms\Components\Combobox;
+use Novadaemon\FilamentCombobox\Combobox;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
