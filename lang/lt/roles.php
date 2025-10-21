75ccd8defcb46783bfcf8ef322705decd556b9b8
Andrej Prus <goleaf@gmail.com>
2025-10-21 16:49:33 +0300

    Add role permissions matrix management

---
+++ lang/lt/roles.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'title' => 'Rolės',
+    'plural' => 'Rolės',
+    'single' => 'Rolė',
+    'navigation' => 'Rolės',
+    'fields' => [
+        'name' => 'Pavadinimas',
+        'guard_name' => 'Sargyba',
+        'permissions_matrix' => 'Leidimai',
+        'permissions_count' => 'Leidimų skaičius',
+    ],
+    'sections' => [
+        'general' => 'Rolės informacija',
+        'permissions' => 'Leidimų matrica',
+    ],
+    'modules' => [
+        'panel' => 'Administratoriaus skydelis',
+        'products' => 'Produktai',
+        'categories' => 'Kategorijos',
+        'brands' => 'Prekių ženklai',
+        'orders' => 'Užsakymai',
+        'users' => 'Vartotojai',
+        'roles' => 'Rolės',
+    ],
+    'abilities' => [
+        'access' => 'Prieiga',
+        'viewAny' => 'Peržiūrėti visus',
+        'view' => 'Peržiūrėti',
+        'create' => 'Kurti',
+        'update' => 'Atnaujinti',
+        'delete' => 'Ištrinti',
+    ],
+];
