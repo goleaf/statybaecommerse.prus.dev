7be46291d8e314dcaf8c05d8b39cc873e9854135
Andrej Prus <goleaf@gmail.com>
2025-10-21 15:12:30 +0300

    Add resizable column support to BaseListRecords (#811)

--- app/Filament/Pages/Support/BaseListRecords.php
+++ app/Filament/Pages/Support/BaseListRecords.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Filament\Pages\Support;
 
 use App\Filament\Tables\Concerns\ConfiguresToggleableTableLayout;
+use Asmit\ResizedColumn\HasResizableColumn;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables\Table;
 use Hydrat\TableLayoutToggle\Concerns\HasToggleableTable;
@@ -12,6 +13,7 @@ use Hydrat\TableLayoutToggle\Concerns\HasToggleableTable;
 abstract class BaseListRecords extends ListRecords
 {
     use ConfiguresToggleableTableLayout;
+    use HasResizableColumn;
     use HasToggleableTable;
 
     public function table(Table $table): Table
