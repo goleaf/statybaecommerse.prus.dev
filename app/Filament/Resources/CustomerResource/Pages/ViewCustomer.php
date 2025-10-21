0287d130e198e85eabc9f134317ad57979192c85
Andrej Prus <goleaf@gmail.com>
2025-10-21 16:03:48 +0300

    Add ListGroup infolist quick links to resource views (#855)

--- app/Filament/Resources/CustomerResource/Pages/ViewCustomer.php
+++ app/Filament/Resources/CustomerResource/Pages/ViewCustomer.php
@@ -7,12 +7,15 @@ namespace App\Filament\Resources\CustomerResource\Pages;
 use App\Filament\Resources\CustomerResource;
 use App\Models\Address;
 use App\Models\Customer;
+use App\Models\Order;
 use Filament\Actions;
 use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
-use Filament\Schemas\Schema;
 use Icetalker\FilamentTableRepeatableEntry\Infolists\Components\TableRepeatableEntry;
+use LaraZeus\ListGroup\Entries\ListItem;
+use LaraZeus\ListGroup\Infolists\ListEntry;
 
 final class ViewCustomer extends ViewRecord
 {
@@ -25,9 +28,60 @@ final class ViewCustomer extends ViewRecord
         ];
     }
 
-    public function infolist(Schema $schema): Schema
+    public function infolist(Infolist $infolist): Infolist
     {
-        return $schema->components([
+        return $infolist->schema([
+            ListEntry::make('customerQuickLinks')
+                ->heading(__('Quick links'))
+                ->state(function (Customer $record): array {
+                    $items = [];
+
+                    if (filled($record->email)) {
+                        $items[] = ListItem::make()
+                            ->id('customer-email-link')
+                            ->label(__('Email :name', ['name' => $record->name]))
+                            ->icon('heroicon-m-envelope')
+                            ->color('primary')
+                            ->url('mailto:' . $record->email)
+                            ->tooltip(__('Compose an email to :email', ['email' => $record->email]))
+                            ->toArray();
+                    }
+
+                    if (filled($record->phone)) {
+                        $items[] = ListItem::make()
+                            ->id('customer-phone-link')
+                            ->label(__('Call :name', ['name' => $record->name]))
+                            ->icon('heroicon-m-phone')
+                            ->color('success')
+                            ->url('tel:' . preg_replace('/[^\d+]/', '', (string) $record->phone))
+                            ->tooltip(__('Dial :phone', ['phone' => $record->phone]))
+                            ->toArray();
+                    }
+
+                    return $items;
+                }),
+            ListEntry::make('customerOrders')
+                ->heading(__('customers.orders'))
+                ->list()
+                ->state(function (Customer $record): array {
+                    $record->loadMissing(['orders']);
+
+                    return $record->orders
+                        ->sortByDesc('created_at')
+                        ->map(function (Order $order): array {
+                            return ListItem::make()
+                                ->id('customer-order-' . $order->getKey())
+                                ->label(__('Order #:number', ['number' => $order->number]))
+                                ->icon('heroicon-m-receipt-percent')
+                                ->color('info')
+                                ->url(route('account.orders.detail', ['number' => $order->number]))
+                                ->tooltip(__('Placed on :date', [
+                                    'date' => optional($order->created_at)->toFormattedDateString(),
+                                ]))
+                                ->toArray();
+                        })
+                        ->all();
+                }),
             Section::make(__('customers.address_information'))
                 ->schema([
                     TableRepeatableEntry::make('addresses')
@@ -38,12 +92,12 @@ final class ViewCustomer extends ViewRecord
 
                             return $record->addresses
                                 ->map(fn (Address $address): array => [
-                                    'name' => $address->full_name,
-                                    'address' => $address->formatted_address ?? $address->address_line_1,
-                                    'city' => $address->city ?: $address->cityById?->name,
+                                    'name'        => $address->full_name,
+                                    'address'     => $address->formatted_address ?? $address->address_line_1,
+                                    'city'        => $address->city ?: $address->cityById?->name,
                                     'postal_code' => $address->postal_code,
-                                    'country' => $address->country?->name ?? $address->country_code,
-                                    'phone' => $address->phone,
+                                    'country'     => $address->country?->name ?? $address->country_code,
+                                    'phone'       => $address->phone,
                                 ])
                                 ->values()
                                 ->all();
