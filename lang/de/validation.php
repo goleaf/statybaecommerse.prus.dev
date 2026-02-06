8cdb661da49272f122081f252d5255ff8526165d
Andrej Prus <andrej.prus@kodinta.lt>
2026-02-06 13:51:23 +0200

    chore: Update existing and add new translations for various application modules across multiple languages.

--- lang/de/validation.php
+++ lang/de/validation.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 return [
-    'accepted'   => 'Das :attribute-Attribut muss akzeptiert werden.',
+    'accepted' => 'Das :attribute-Attribut muss akzeptiert werden.',
     'active_url' => 'validation.active_url',
-    'unique'     => 'Unique',
+    'unique' => 'Unique',
+    'after' => 'The :attribute must be a date after :date.',
 ];
